dca0a3d31066660c3a130d77f1e39285d2300e7d
IgnaDev <arcortoon@gmail.com>
2023-08-03 10:03:00 -0300

    Reto completado

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -2,9 +2,17 @@ import{ useState } from 'react'
 
 export default function GifExpertApp() {
     const [categories, setCategories] = useState(['One Punch', 'Samurai X', 'Dragon Ball'])
+
+    const onAddCategory = (nuevaCategoria) => {
+        setCategories([...categories, nuevaCategoria])
+    }
   return (
     <div>
         <h1>GifExpertApp</h1>
+        <button onClick={()=>{
+            let nuevaCategoria = prompt('Ingrese una nueva categoria')
+            onAddCategory(nuevaCategoria)
+        }}>Agregar</button>
         <ol>
             {
                 categories.map(category => {
