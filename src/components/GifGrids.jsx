19b861704819d04514763faa1f4be66ff18d4053
IgnaDev <arcortoon@gmail.com>
2023-08-04 10:33:54 -0300

    Conectado con la API y uso de useEffect

--- src/components/GifGrids.jsx
+++ src/components/GifGrids.jsx
@@ -1,22 +1,32 @@
 import PropTypes from 'prop-types';
 import getGifs from '../helpers/getGifs';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 
 
 
 export default function GifGrids({category}){
-   
+    const [images, setImages] = useState([])
+
     useEffect(() => {
         getGifs(category)
-            .then(imgs => console.log(imgs))
+            .then(imgs => setImages(imgs))
     },[])
    
     
     return (
         <>
             <h3>{category}</h3>
-
+            <ol>
+                {images.map(img =>
+                    (<li key={img.id}>
+                        <p>{img.title}</p>
+                        <img src={img.url}  />
+                    </li>
+                    )
+                    
+                    )}
+            </ol>
         </>
     )
 }
--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,5 @@
 const getGifs = async (category) => {
-    const url = "https://api.giphy.com/v1/gifs/search?api_key=40EfGMu3Ck4f8KeR9ubEymkyrvLCa2aE&q="+ category+"&limit=20" ;
+    const url = "https://api.giphy.com/v1/gifs/search?api_key=40EfGMu3Ck4f8KeR9ubEymkyrvLCa2aE&q="+ category+"&limit=10" ;
     const resp = await fetch(url);
     const data = await resp.json();
 
